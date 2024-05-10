ba9d789a4a98b111970f9e2f88c7ea68f6c86fd1
Joaquín A Gandini <joaquingandini22@gmail.com>
2024-05-10 13:49:31 -0300

    finish

--- scenes/Game.js
+++ scenes/Game.js
@@ -26,7 +26,7 @@ export default class Game extends Phaser.Scene {
     this.load.image("star", "../public/assets/star.png")
 
     //import bomba
-    this.load.image("circle","../public/assets/circle.png")
+    this.load.image("enemigo","../public/assets/circle.png")
   }
 
   create() {
@@ -34,9 +34,9 @@ export default class Game extends Phaser.Scene {
     
     //map
 
-  // const map = this.add.tilemap("map");
-  // const tiles = map.addTilesImage("main","tiles")
-
+    // const map = this.add.tilemap("map");
+    // const tiles = map.addTilesImage("main","tiles")
+    
     //animaciones personaje
     //this.anims.create({
     //  key: "idle",
@@ -49,35 +49,46 @@ export default class Game extends Phaser.Scene {
     const cielo = this.add.image(400, 300, "cielo");
     cielo.setScale(2);
 
+    //agregar puntucación 
+    score = 0
+    // crear el texto de puntos, con sus propiedades; Inicializamos en 0
+    txtScore = this.add.text(10,20,"Score: 0",{font:"20px Helvetica", fill:"#ff0000"})
+
     //crear plataforma
     plataformas = this.physics.add.staticGroup();
-    plataformas.create(400, 580, "plataforma").setScale(1.8,1.3).setSize(720,45).setOffset(-160,-5) // Size tamaño, y offset posición x e y
+    plataformas.create(400, 580, "plataforma").setScale(2,1.3).setSize(800,45).setOffset(-200,-5) // Size tamaño, y offset posición x e y
     plataformas.create(200, 300, "plataforma").setScale(0.5,0.9).setSize(200,28).setOffset(100,2)
     plataformas.create(600, 400, "plataforma").setScale(0.5,0.9).setSize(200,28).setOffset(100,2)
 
-    //agregar estrellas
+    // Bolsa de todas las estrellas, grupo
     allStars = this.physics.add.group({
-      key: 'star',
-      repeat: 8,
-      setScale: {x: 0.02, y: 0.02},
-      setOffset: {x:3, y:3},
-      setXY: { x: 110, y: 120, stepX: 80 }
-  });
+      key: 'star', // nombre del hijo unidad
+      repeat: 8,  // cuantas veces se repiten más
+      setScale: {x: 0.02, y: 0.02}, // escalado
+      setXY: { x: 110, y: 210, stepX: 80 }  // Posición X e Y e distanciamiento stepsX
+    });
   
-  allStars.children.iterate(function (child) {
-  
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-  
-  });
-    
-  
+    // Llamar a los hijos
+    allStars.children.iterate(function (child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8)); // Grupo de Stars que reboten, entre la 0.4 y 0.8 de su salto altura
+    });
 
-    //agregar puntucación 
-    score = 0
-
-    //agregar "obstáculos"
-    circle = this.physics.add.sprite(300,-10, "circle").setScale(0.01,0.01)
+    //agregar enemigos, bolsas
+    enemigos = this.physics.add.group({
+      key: 'enemigo',
+      repeat: 4,
+      setScale: {x:0.01, y:0.01},
+      setXY: {x: 40, y:-10, stepX:(numR)}
+    })
     
+    // Llamar a los hijos, recorrer todos los enemigos del grupo
+    enemigos.children.iterate(function (enemigo){
+      enemigo.setBounceY(1.01)
+      enemigo.setVelocity(0)
+      enemigo.setCollideWorldBounds()
+    })
+    
+
     //agregar personaje
     personaje = this.physics.add.sprite(400, 500, "personaje");
     personaje.setScale(0.08);
@@ -90,14 +101,12 @@ export default class Game extends Phaser.Scene {
 
     // colisiones  // agregar eventos ??
     this.physics.add.collider(plataformas, personaje); // colision entre plataforma y personaje
-    this.physics.add.collider(circle, personaje, personajeDamage, null, this); // colisión entre circle y personaje 
-    this.physics.add.collider(circle, plataformas) // colisión entre circle y plataformas
-    this.physics.add.collider(personaje, plataformas); // colision entre personaje y plataforma 
-    
-    this.physics.add.collider(allStars, plataformas);
-    this.physics.add.overlap(personaje, allStars, pickPowerUp, null, this);
+    this.physics.add.collider(plataformas, enemigos) // colisión entre circle y plataformas
+    this.physics.add.collider(plataformas, allStars);
+    this.physics.add.collider(enemigos, enemigos);
 
-
+    this.physics.add.overlap(personaje, allStars, pickStar, null, this);
+    this.physics.add.collider(personaje, enemigos, gameOver, null, this); // entre circle y personaje 
   }
 
   update() {
@@ -133,21 +142,38 @@ export default class Game extends Phaser.Scene {
   }
 }
 
+var score;
+var txtScore;
+var enemigos;
+var allStars;
+var personaje;
+var plataformas;
+var cursors;
+var star;
 
-var circle
-var score
-var allStars
-var personaje
-var plataformas
-var cursors
-var dmg
-var Over
 
-function personajeDamage(dmg) {
-      scene: [retry]
-  return dmg
+const numR = Math.floor(Math.random() * 260) + 100
+    console.log(numR)
+
+function pickStar (personaje, star) {
+  // allStars.destroy() // Destruye las estrellas
+  star.disableBody(true,true); // Desactiva las estrellas, Recomendado
+  
+  score = score + 100; // suma puntos
+  console.log(score);
+  txtScore.setText("Score: " + score)
+
+  console.log(allStars.countActive()); // Funciona?, muestra en consola la cantidad de monedas
+  if (allStars.countActive() === 0) {
+    
+    // recorrer el monedero
+    allStars.children.iterate(function (child) {
+      child.enableBody(true, child.x,100,true,true)
+  });
+  }
 }
 
-function pickPowerUp (personaje, allStars) {
-  allStars.disableBody(true,true);
+function gameOver () {
+  this.game.pause()
 }
+
